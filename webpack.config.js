a4ddf3adb6ab8ecc84e300415771806a3bec1488
andres-mestra <andresmestra11a@gmail.com>
2021-09-01 11:15:14 -0500

    Configuración inicial del proyecto

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlagin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  output: {
+    filename: "app.bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlagin({
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
