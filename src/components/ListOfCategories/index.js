d2868132488649af8406f854d9d2956234567cbd
andres mestra <andresmestra11a@gmail.com>
2021-09-02 17:03:14 -0500

    List categories fixed and event scroll

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useFetch } from '../../hooks/useFetch'
 import { Category } from '../Category'
 import { Item, List } from './styles'
@@ -11,16 +11,35 @@ export const ListOfCategories = () => {
     error,
   } = useFetch('https://petgram-server-mestra.vercel.app/categories')
 
-  if (loading) return <p>loadin...</p>
-  if (error) return <p>!Error!</p>
+  const [showFixed, setShowFixed] = useState(false)
 
-  return (
-    <List>
+  useEffect(() => {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed && 'fixed'}>
       {categories?.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  )
+
+  if (loading) return <p>loadin...</p>
+  if (error) return <p>!Error!</p>
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
