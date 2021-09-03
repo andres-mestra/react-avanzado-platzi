fbd37c6d8d1a24d40cd23ebbce3d2f51c1945609
andres mestra <andresmestra11a@gmail.com>
2021-09-02 21:53:59 -0500

    new component FavButton

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -2,8 +2,8 @@ import * as React from 'react'
 import { useLocalStorage } from '../../hooks/useLocaStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { singlePhotoQuery } from '../../queries/singlePhotoQuery'
-import { Article, Button, Img, ImgWrapper } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { FavButton } from '../FavButton'
+import { Article, Img, ImgWrapper } from './styles'
 
 const DEFAULT_IMAGE =
   'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
@@ -13,7 +13,9 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
 
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
+  const handleFavClick = () => {
+    setLiked(!liked)
+  }
 
   return (
     <Article ref={element}>
@@ -24,10 +26,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size="32px" />
-            {likes} likes!
-          </Button>
+          <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
         </>
       )}
     </Article>
