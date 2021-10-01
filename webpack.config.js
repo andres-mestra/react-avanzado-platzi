e0890a89c4cee5484a8306387b6ba49c74e344c0
andresdev <andresmestra11a@gmail.com>
2021-10-01 18:49:10 -0500

    feat(pwa manifest): initial manifest for PWA funtionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlagin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const path = require('path')
 
 module.exports = {
   mode: 'development',
@@ -9,6 +11,19 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlagin({
       template: 'src/index.html',
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      shortname: 'Petgram 🐶',
+      description: 'Con Petgram puedes encontrar fotos de animales domesticos',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+      ],
     }),
   ],
   module: {
