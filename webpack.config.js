760fce41445cef818a2b6111e0d93e2590f14ae9
andresdev <andresmestra11a@gmail.com>
2021-10-01 19:13:22 -0500

    feat(service worker): config service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlagin = require('html-webpack-plugin')
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -18,10 +19,35 @@ module.exports = {
       description: 'Con Petgram puedes encontrar fotos de animales domesticos',
       background_color: '#fff',
       theme_color: '#b1a',
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: '/',
+      scope: '/',
       icons: [
         {
           src: path.resolve('src/assets/icon.png'),
           sizes: [96, 128, 192, 256, 384, 512],
+          ios: true,
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            'https://res.cloudinary.com|images.unsplash.com'
+          ),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images',
+          },
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-mestra.vercel.app/'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api',
+          },
         },
       ],
     }),
